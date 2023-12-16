043f96c1b14c9f5e8b32846d376c712a966b30e9
muhammadthoriq702 <muhammadthoriq702@upi.edu>
2023-12-16 23:16:03 +0700

    Tampilan Login

--- screens/Login.js
+++ screens/Login.js
@@ -1,149 +1,84 @@
 import React, { useState } from 'react';
-
-import { Image, View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
-
- 
+import { ImageBackground, Image, View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
 const Login = () => {
-
   const [email, setEmail] = useState('');
-
   const [password, setPassword] = useState('');
 
- 
-
   const handleLogin = () => {
-
     console.log('Email:', email);
-
     console.log('Password:', password);
-
   };
 
- 
-
   return (
-
-    <View style={[styles.container, { backgroundColor: '#528BF9' }]}>
-
-      <Image
-
-        source={require('../assets/BeKi.png')} 
-
-        style={styles.logo}
-
-      />
-
-      <TextInput
-
-        style={[styles.input, { borderRadius: 10 }]}
-
-        placeholder="Email"
-
-        onChangeText={(text) => setEmail(text)}
-
-      />
-
-      <TextInput
-
-        style={[styles.input, { borderRadius: 10 }]}
-
-        placeholder="Password"
-
-        secureTextEntry
-
-        onChangeText={(text) => setPassword(text)}
-
-      />
-
-      <TouchableOpacity style={styles.button} onPress={handleLogin}>
-
-        <Text style={styles.buttonText}>Login</Text>
-
-      </TouchableOpacity>
-
-    </View>
-
+    <ImageBackground
+      source={require('../assets/BG-LOGIN.jpg')}
+      style={styles.container}
+    >
+      <View style={styles.overlay}>
+        <Image
+          source={require('../assets/Logo Dlillah.png')}
+          style={styles.logo}
+        />
+        <TextInput
+          style={[styles.input, { borderRadius: 10 }]}
+          placeholder="Email"
+          onChangeText={(text) => setEmail(text)}
+        />
+        <TextInput
+          style={[styles.input, { borderRadius: 10 }]}
+          placeholder="Password"
+          secureTextEntry
+          onChangeText={(text) => setPassword(text)}
+        />
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
-
 };
 
- 
-
 const styles = StyleSheet.create({
-
   container: {
-
     flex: 1,
-
     justifyContent: 'center',
-
     alignItems: 'center',
-
     padding: 16,
-
   },
-
+  overlay: {
+    backgroundColor: 'rgba(255, 255, 255, 0.8)', // Warna putih dengan tingkat transparansi 0.8
+    padding: 16,
+    borderRadius: 10,
+    width: '80%', // Sesuaikan dengan kebutuhan
+    alignItems: 'center', // Agar kontennya berada di tengah secara horizontal
+  },
   logo: {
-
-    width: 120,
-
-    height: 160, 
-
-    marginBottom: 16,
-
+    width: 250,
+    height: 100,
+    marginBottom: 10,
+    resizeMode: 'contain', // Menggunakan 'contain' agar gambar tetap proporsional di dalam kotak
   },
-
-  title: {
-
-    fontSize: 24,
-
-    fontWeight: 'bold',
-
-    marginBottom: 16,
-
-  },
-
   input: {
-
     width: '100%',
-
     height: 40,
-
     borderColor: 'gray',
-
     borderWidth: 1,
-
     marginBottom: 16,
-
     padding: 8,
-
   },
-
   button: {
-
     backgroundColor: 'white',
-
     padding: 10,
-
+    marginBottom: 15,
     borderRadius: 5,
-
   },
-
   buttonText: {
-
     color: '#528BF9',
-
     fontSize: 16,
-
     fontWeight: 'bold',
-
     textAlign: 'center',
-
   },
-
 });
-
- 
 
 export default Login;
